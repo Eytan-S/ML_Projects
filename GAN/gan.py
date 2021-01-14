025c03f54a61b0796dd251ffd0234b16e486fd88
Eytan Scemama <eytan.scemama@gmail.com>
2021-01-14 13:42:50 +0200

    GAN from original paper with the following changes: normalized data to -1 to 1 anf changed generator final activation to tanh changed all activations to leaky relu changed loss function fake <-> real for quick generator training added dropout in descriminator added batchnorm to generator used adam optimization

--- GAN/gan.py
+++ GAN/gan.py
@@ -1,7 +1,131 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec 16 16:21:26 2020
-bla bla
-@author: Eytan
-"""
+import numpy as np
+import random
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
+
+# Get data from cifar10
+
+(x_train, _), (x_test, _) = tf.keras.datasets.cifar10.load_data()
+
+# Pre-proccess  data
+
+x_train = x_train.reshape(x_train.shape[0],-1)
+x_train = x_train.astype(np.float32)/255.0
+x_train = x_train * 2 -1
+
+
+def disc():
+    """
+    Keres model, computes discriminator score
+    
+    Returns
+    tensor with with batch_size score 
+    """
+    
+    model = keras.Sequential()
+    model.add(keras.layers.Dense(512, input_shape=(32*32*3,)))
+    model.add(keras.layers.LeakyReLU())
+    model.add(keras.layers.Dropout(rate=0.5))
+    model.add(keras.layers.Dense(512))
+    model.add(keras.layers.LeakyReLU())
+    model.add(keras.layers.Dropout(rate=0.5))
+    model.add(keras.layers.Dense(512))
+    model.add(keras.layers.LeakyReLU())
+    model.add(keras.layers.Dropout(rate=0.5))
+    model.add(keras.layers.Dense(1))
+
+    model.optimizer = keras.optimizers.Adam(learning_rate=0.0001)
+    
+    return model
+
+def gen(noise_size):
+    """
+    Keres model, generates image
+    
+    Iputs:
+        tensor with batch_size noise
+    Returns
+        tensor with with batch_size images
+    """
+
+    model = keras.Sequential()
+    model.add(keras.layers.Dense(1024, input_shape=(noise_size,)))
+    model.add(keras.layers.LeakyReLU())
+    model.add(keras.layers.BatchNormalization(momentum=0.8))
+    model.add(keras.layers.Dense(1024))
+    model.add(keras.layers.LeakyReLU())
+    model.add(keras.layers.BatchNormalization(momentum=0.8))
+    model.add(keras.layers.Dense(1024))
+    model.add(keras.layers.LeakyReLU())
+    model.add(keras.layers.BatchNormalization(momentum=0.8))
+    model.add(keras.layers.Dense(32*32*3, activation='tanh'))
+             
+    model.optimizer = keras.optimizers.Adam(learning_rate=0.0001)
+    
+    return model
+
+
+# Set hyper-parameters
+num_epochs = 100
+num_iterations = 500
+batch_size = 100
+noise_size = 100
+K = 1
+
+# Models
+
+G = gen(noise_size)
+D = disc()
+images = []
+disc_losses = []
+gen_losses = []
+
+# Training
+
+
+for epoch in range(num_epochs):
+    for batch in range(num_iterations):
+        for k in range(K):
+            with tf.GradientTape() as disc_tape:
+                
+                idx = random.randint(0, x_train.shape[0]-batch_size)
+                real_data = x_train[idx:idx+batch_size]
+                real_results = D(real_data)
+                
+                disc_noise = tf.random.uniform((batch_size, noise_size), minval=-1, maxval=1)
+                disc_fake_images = G(disc_noise)
+                fake_results = D(disc_fake_images)
+                
+                disc_loss = (keras.losses.binary_crossentropy(np.ones((batch_size,1)), real_results, from_logits='true') + keras.losses.binary_crossentropy(np.zeros((batch_size,1)), fake_results, from_logits='true')) / 2
+                disc_grads = disc_tape.gradient(disc_loss, D.trainable_variables)
+                
+                D.optimizer.apply_gradients(zip(disc_grads, D.trainable_variables))
+            
+        with tf.GradientTape() as gen_tape:
+            
+            gen_noise = tf.random.uniform((batch_size, noise_size), minval=-1, maxval=1)
+            gen_fake_images = G(gen_noise)
+            gen_fake_results = D(gen_fake_images)
+            
+            gen_loss = keras.losses.binary_crossentropy(np.ones((batch_size, 1)), gen_fake_results, from_logits='true')
+            gen_grads = gen_tape.gradient(gen_loss, G.trainable_variables)
+            
+            G.optimizer.apply_gradients(zip(gen_grads, G.trainable_variables))
+        if (batch % 50 == 0):
+            print("Epoch: %d, Batch: %d, D loss: %s, G loss: %s" % (epoch, batch, np.mean(disc_loss.numpy()), np.mean(gen_loss.numpy())))
+            noise = tf.random.uniform((1, noise_size), minval=0, maxval=1)
+            img = G(noise)
+            np_img = ((img.numpy().reshape(32,32,3) + 1) / 2 * 255).astype(int)
+            #plt.imshow(np_img)
+            images.append(np_img)
+            disc_losses.append(np.mean(disc_loss.numpy()))
+            gen_losses.append(np.mean(gen_loss.numpy()))
+            
+            
+        if np.mean(gen_loss.numpy()) < 0.2: break
+                  
+        
+            
